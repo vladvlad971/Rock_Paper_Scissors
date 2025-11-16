feb9e37ea411d2a47f53c4f260b064f2868f62de
vladvlad971 <vlad.salomont@gmail.com>
2025-11-16 19:32:59 +0100

    Writing the pseudocode
    drafting the steps that the program will have to perform

---
+++ script.js
@@ -0,0 +1,22 @@
+/*
+- Write the logic to get the computer choice.
+CREATE function getComputerChoice;
+DISPLAY rock, paper or scissors randomly;
+TEST whit console.log;
+- Write the logic to get the human choice.  
+CREATE function getHumanChoice
+GET the input of the human player (prompt);
+RETURN the input whit console.log;
+- Write the logic to play a single round.
+CREATE function playRound
+SET humanChoice and computerChoice variable while be used to parameter;
+SET the output of variables to case-insensitive such .toUpperCase;
+WRITE the code that check the win condition (if/else);
+DISPLAY the string value representing the round winner;
+- Write the logic to play the entire game.
+CREATE function playGame;
+MOVE playRound function in playGame func;
+CALL the function 5 times;
+*/
+
+
