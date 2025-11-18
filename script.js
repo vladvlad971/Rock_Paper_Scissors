3f56b962d7049540c8850447a888599c17f80482
vladvlad971 <vlad.salomont@gmail.com>
2025-11-18 22:45:54 +0100

    Writing the playRound function and brake program fix due to logs
    Implementation of the `ef`/`else` statements and return values. Removal of previous logs that were causing the function outputs to become false.

--- script.js
+++ script.js
@@ -19,38 +19,43 @@ MOVE playRound function in playGame func;
 CALL the function 5 times; alert
 */
 
-const choiceString = ["ROCK","PAPER","SCISSORS"] ;
+let choiceString = ["ROCK","PAPER","SCISSORS"] ;
 
 //output computer choice.
 function getComputerChoice(choiceString){
    let selectString = Math.floor(Math.random()*3);
-   return choiceString[selectString];
+   return choiceString[selectString] ;
 }
-console.log(getComputerChoice(choiceString))
-
-let humanInput =prompt("choose between ROCK, PAPER or SCISSORS");
-humanInput = humanInput.toUpperCase();
 
 //output human choice.
+let humanInput = prompt("choose between ROCK, PAPER or SCISSORS");
+
+
 function getHumanChoice(humanInput) {
-   return humanInput;
+    humanInput = humanInput.toUpperCase();
+    return humanInput ;
 }
-console.log(getHumanChoice(humanInput))
 
+//Score variable
+let humanScore = 0;
+let computerScore = 0;
 //play a single round.
-function playRound(computerChoice,humanChoice) {
-
-   
-  if ((computerChoice === choiceString[0]) && (humanChoice === choiceString[2])) {
-      return true ;
-  } else if ((computerChoice === choiceString[1]) && (humanChoice === choiceString[0])){
-      return true;
-  } else if ((computerChoice === choiceString[2]) && (humanChoice === choiceString[1])){
-      return true;
-  } 
-}
+let humanChoice = getHumanChoice(humanInput);
 let computerChoice = getComputerChoice(choiceString);
-let humanChoice = getHumanChoice(humanInput)
-console.log(playRound(computerChoice,humanChoice));
 
-//récupérer les output humain et machine, les convertir en numérique, comparer les forces et faiblesse, retourner le gagnant
+function playRound(humanChoice,computerChoice) {
+    if ((humanChoice === choiceString[0]) && (computerChoice === choiceString[2])) {
+      return `You win, ${humanChoice} takes over ${computerChoice}!`;
+  } else if ((humanChoice === choiceString[1]) && (computerChoice === choiceString[0])){
+      return `You win, ${humanChoice} takes over ${computerChoice}!`
+  } else if ((humanChoice === choiceString[2]) && (computerChoice === choiceString[1])){
+      return `You win, ${humanChoice} takes over ${computerChoice}!`
+  } else if (humanChoice === computerChoice) {
+    return `Equality, ${humanChoice} match ${computerChoice}!`
+  } else {
+    return `You lose, ${humanChoice} lose the round versus ${computerChoice}!`  
+  }
+}
+
+
+console.log(playRound(humanChoice,computerChoice))
